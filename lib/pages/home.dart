cf03a0fa630a89a02437f3bc741288e7107a4a60
mamerida <mamerida2018@gmail.com>
2023-10-07 19:30:27 -0300

    extract in method

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,59 +9,68 @@ const HomePage({ Key? key }) : super(key: key);
   Widget build(BuildContext context){
     return Scaffold(
       appBar: Appbar(),
+      backgroundColor: Colors.white,
       body:Column(
         children: [
-          Container(
-            margin: EdgeInsets.only(top:40,left: 20,right: 20),
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: const Color(0xff1D1617).withOpacity(0.11),
-                  blurRadius: 40,
-                  spreadRadius: 0.0
-                )
-              ]
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: EdgeInsets.all(15),
-                hintText: "Search Mate",
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: SvgPicture.asset("assets/icons/Search.svg"),
-                ),
-                suffixIcon: Container(
-                  width: 100,
-                  child: IntrinsicHeight(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        VerticalDivider(
-                            color: Colors.black,
-                            indent: 10,
-                            endIndent: 10,
-                            thickness: 0.1,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(12),
-                          child: SvgPicture.asset("assets/icons/Filter.svg"),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: BorderSide.none
-                )
-              ),
-            ),
-          )
+          _searchField()
         ],
       )
     );
+  }
+
+  Container _searchField() {
+    return Container(
+          margin: EdgeInsets.only(top:40,left: 20,right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: const Color(0xff1D1617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0
+              )
+            ]
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: "Search Mate",
+              hintStyle: const TextStyle(
+                color: Color(0xffDDDADA),
+                fontSize: 14
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset("assets/icons/Search.svg"),
+              ),
+              suffixIcon: Container(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      VerticalDivider(
+                          color: Colors.black,
+                          indent: 10,
+                          endIndent: 10,
+                          thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: SvgPicture.asset("assets/icons/Filter.svg"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none
+              )
+            ),
+          ),
+        );
   }
 
   AppBar Appbar() {
